0ac39b5a24955b11f40b865229610426084b3ec6
cwong2015 <sirhc8002@gmail.com>
2018-01-01 17:27:32 -0500

    fixed a bug with redirection

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var authRoute = require('./routes/authRoute');
 var passportSetup = require('./config/passport-setup');
+var keys = require('./config/keys');
+var cookieSession = require('cookie-session');
+var passport = require('passport');
 
 var app = express();
 app.set('view engine', 'ejs');
+
+// app.use(cookieSession({
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: [keys.session.cookieKey]
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+app.use('/auth', authRoute);
 
 app.use('/assets', express.static('./assets'));
 
 app.get('/', (req, res)=>{
     res.render('home');
 });
-
-app.use('/auth', authRoute);
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000');
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,12 +2,21 @@ var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20');
 var keys = require('../config/keys');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
     callbackURL: '/auth/google/redirect',
     clientID: keys.keys.clientId,
     clientSecret: keys.keys.secretKey
     }, (accessToken, refreshToken, profile, done)=> {
-        console.log(profile);
     })
 );
