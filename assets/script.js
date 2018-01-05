39baa7332da48fbf3c2e8382670f77226b2461fd
cwong2015 <sirhc8002@gmail.com>
2018-01-05 17:57:23 -0500

    got something working with the add/removing poll options using jQuery

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,58 @@
+function remove(arr, element){
+    var index = arr.indexOf(element);
+    if (index !== -1) {
+        arr.splice(index, 1);
+    }
+}
+
 $(document).ready(function(){
+    var split1 = "<div><input class = 'textbox' type = 'text' name = 'option";
+    var split2 = "' placeholder = 'Option...' /></input>";
+    var split3 = " <input type = 'button' id = 'option";
+    var split4 = "' value = 'x'/></div>";
+    var btnNum = 2;
+
+    var arr = [];
+    arr.push("option1", "option2");
+
+    $("#option1").on('click', ()=>{
+        $('[name=option1]').remove();
+        $("#option1").remove();
+        remove(arr, "option1");
+        btnNum--;
+        console.log(arr);
+    });
+
+    $("#option2").on('click', ()=>{
+        $('[name=option2]').remove();
+        $("#option2").remove();
+        remove(arr, "option2");
+        btnNum--;
+        console.log(arr);
+    });
 
     $('form').on('submit', function(){
         console.log('this will eventually submit this poll to datbase');
     });
 
     $('#mainPoll').on('click', function(){
-        $('#poll').append("<div><input class = 'textbox' type = 'text' name = 'otherOption' placeholder = 'Option...' /></input></div>")
+        btnNum++;
+        $('#poll').append(split1 + btnNum + split2 + split3 + btnNum + split4);
+        arr.push("option" + btnNum);
+        for(var i = 0; i < arr.length; i ++){
+            var events = $._data(document.getElementById("option" + (i+1)), "events");
+            var hasEvents = (events != null);
+            var ref;
+            if(!hasEvents){
+                ref = "option" + (i+1);
+                $("#" + arr[i]).on('click', ()=>{
+                    $('[name=' + ref + ']').remove();
+                    $("#" + ref).remove();
+                    remove(arr, ref);
+                    btnNum--;
+                    console.log(arr);
+                });
+            }
+        }
     });
 });
